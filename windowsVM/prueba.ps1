78cee3e0cf535e8a95eade6e7b5d3bce7a2cbb77
Karol <annaosro2005@gmail.com>
2026-02-17 17:27:24 -0700

    pruebas 2

--- windowsVM/prueba.ps1
+++ windowsVM/prueba.ps1
@@ -1,4 +1,4 @@
-$Old = Get-DnsServerResourceRecord -ZoneName "zona.com" -Name "nombre" -RRType "A"
+$Old = Get-DnsServerResourceRecord -ZoneName "prueba.com.dns" -Name "prueba.com" -RRType "A"
 $New = $Old.Clone()
 $New.RecordData.IPv4Address = [System.Net.IPAddress]::Parse("nueva.ip")
-Set-DnsServerResourceRecord -OldInputObject $Old -NewInputObject $New -ZoneName "zona.com"   
+Set-DnsServerResourceRecord -OldInputObject $Old -NewInputObject $New -ZoneName "prueba.com.dns"   
