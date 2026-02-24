867b519d90693e8ea34797704fc924e48a320cab
krolazepam <annaosro2005@gmail.com>
2026-02-24 14:20:16 -0700

    creacion del script .ps1

---
+++ windowsVM/tarea4.ps1
@@ -0,0 +1,12 @@
+function mostrarMenu{
+    Write-Host "----------------------------------" -ForegroundColor Magenta
+    Write-Host "   Menu  " -ForegroundColor Magenta
+    Write-Host "----------------------------------" -ForegroundColor Magenta
+    Write-Host "1. " 
+    Write-Host "2. " 
+    Write-Host "3. "
+    Write-Host "4. "
+    Write-Host "5. " 
+    Write-Host "6. Salir" 
+    Write-Host "----------------------------------" -ForegroundColor Magenta
+}
