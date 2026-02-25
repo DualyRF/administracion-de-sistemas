f2ab285e1af5d352b377e56b682c525a3b1884be
krolazepam <annaosro2005@gmail.com>
2026-02-25 14:32:01 -0700

    subprograma 1

---
+++ openSUSE/librerias/varios.sh
@@ -0,0 +1,24 @@
+# colores en variables
+rojo='\033[0;31m'
+amarillo='\033[1;33m'
+verde='\033[0;32m'
+azul='\033[1;34m'
+cyan='\033[0;36m'
+nc='\033[0m'
+
+# Funciones para imprimir mensajes 
+print_warning(){
+    echo -e "${rojo}$1${nc}"
+}
+
+print_success(){
+    echo -e "${verde}$1${nc}"
+}
+
+print_info(){
+    echo -e "${amarillo}$1${nc}"
+}
+
+print_menu(){
+    echo -e "${cyan}$1${nc}"
+}
