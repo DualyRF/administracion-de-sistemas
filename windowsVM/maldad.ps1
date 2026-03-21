149980d48b1939453b8c37604932a5ed23c0ff0d
Karol <annaosro2005@gmail.com>
2026-03-21 15:27:52 -0700

    pruebas 7 (no) me voy a dar de baja

--- windowsVM/maldad.ps1
+++ windowsVM/maldad.ps1
@@ -1,2 +1,5 @@
 $archivo = "C:\Users\Administrador\Desktop\administracion-de-sistemas\windowsVM\practica8\tarea8.ps1"
-Select-String -Path $archivo -Pattern "SoftLimit"
+
+(Get-Content $archivo) -replace '-Active\s+\$true', '-Active' | Set-Content $archivo
+
+Write-Host "Corregido." -ForegroundColor Green
